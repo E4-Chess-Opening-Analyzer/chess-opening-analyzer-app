39001989243a57034c5f22b567a349caf89e147e
Adrien Panis <adrien.panis@my-goodlife.fr>
2025-11-23 16:23:11 +0100

    fix: linter errors

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:app/states/moves/moves_cubit.dart';
 import 'package:app/views/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nested/nested.dart';
+import 'package:provider/single_child_widget.dart';
 
 void main() {
   runApp(const MyApp());
